1a98e05852d33773c4d69dc050fe0b88543743d1
Intermaze <rafikiprawhira@gmail.com>
2024-03-17 11:01:28 +0700

    feat: iterations input

--- src/GUI.py
+++ src/GUI.py
@@ -1,7 +1,8 @@
-import tkinter
+import tkinter as tk
 from BezierDivideAndConquer import *
 
-root = tkinter.Tk()
+root = tk.Tk()
+root.title('Bezier Curve Simulation with Divide and Conquer')
 
 points = Line() # List of points
 
@@ -15,19 +16,32 @@ def recursive_draw(start,color):
 def key(event):
     print ("pressed", repr(event.char))
 
-def callback(event):
+def click(event):
     print ("clicked at", event.x, event.y)
     points.pushback(Point(event.x,event.y))
+    redrawCurve()
+
+def redrawCurve():
     bezier = Line()
-    bezierDivConquer(bezier,points,12,0)
+    bezierDivConquer(bezier,points,int(iterations.get()),0)
     canvas.delete("all")
     recursive_draw(points.head,"red")
     recursive_draw(bezier.head,"blue")
-    
 
-canvas= tkinter.Canvas(root, width=800, height=600)
-canvas.bind("<Key>", key)
-canvas.bind("<Button-1>", callback)
+canvas= tk.Canvas(root, width=800, height=600)
+
 canvas.pack()
+
+frame = tk.LabelFrame(root, text='Options', padx=10, pady=5)
+frame.place(in_=canvas, relx=1, rely=0, anchor='ne')
+
+label = tk.Label(frame, text='Iterations: ')
+label.pack()
+iterations = tk.StringVar()
+spinbox = tk.Spinbox(frame, command=redrawCurve, from_=1.0, to=12.0, textvariable=iterations, width=8, wrap=True)
+spinbox.pack()
+
+canvas.bind("<Key>", key)
+canvas.bind("<Button-1>", click)
 
 root.mainloop()
